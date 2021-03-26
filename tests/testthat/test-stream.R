2d47c4c59c0b159e26273fea9ae245ba5747e3d7
Matthew Talluto <matthew.talluto@uibk.ac.at>
2021-03-26 12:28:21 +0100

    Version update

--- tests/testthat/test-stream.R
+++ tests/testthat/test-stream.R
@@ -7,7 +7,7 @@ test_that("r.watershed", {
 	expect_error(kamp_sm <- delineate(kamp_dem_sm), regex=NA)
 	expect_error(kamp_sm2 <- delineate(kamp_dem_sm, reach_len = 200), regex=NA)
 	expect_lt(length(raster::unique(kamp_sm$stream)), length(raster::unique(kamp_sm2$stream)))
-	expect_error(kv <- vectorise_stream(kamp_sm[['stream']]), regex=NA)
+	expect_error(kv <- vectorise_stream(kamp_sm[['stream']], pixel_topology(kamp_sm)), regex=NA)
 	expect_true(all(kv$reach_id %in% raster::unique(kamp_sm$stream)))
 
 	## reach resizing
